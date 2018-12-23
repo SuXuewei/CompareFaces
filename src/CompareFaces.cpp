63589e2b5634e884f976da257f021bd79bd67ab8
g_su_xw <g_su_xw@163.com>
2018-12-23 22:30:38 +0800

    提取SIFT特征点，做图像比对

--- src/CompareFaces.cpp
+++ src/CompareFaces.cpp
@@ -3,16 +3,17 @@
 // Author      : g_su_xw
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Ansi-style
 //============================================================================
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/nonfree/features2d.hpp"
 
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
 using namespace cv;
@@ -22,38 +23,89 @@ const string WINDOWNAME_FACE2 = "Face Detection Face 2";
 
 int matchFaces(const Mat &face1, const Mat &face2)
 {
-	return 1;
-//    cv::initModule_nonfree();
-//    Mat img1_mat = face1;
-//    Mat img2_mat = face2;
-//
-//    std::vector<KeyPoint> img1_keypoint,img2_keypoint;
-//    /*特征点检测*/
-//    //  FeatureDetector *sift_detector = FeatureDetector::create("SIFT");
-//    Ptr<FeatureDetector> sift_detector = FeatureDetector::create("SIFT");
-//    sift_detector->detect(img1_mat,img1_keypoint);
-//    sift_detector->detect(img2_mat,img2_keypoint);
-//    /*在图片上显示特征点*/
-//    Mat img_kp_1,img_kp_2;
-//    drawKeypoints(img1_mat,img1_keypoint,img_kp_1,Scalar::all(-1),DrawMatchesFlags::DEFAULT);
-//    drawKeypoints(img2_mat,img2_keypoint,img_kp_2,Scalar::all(-1),DrawMatchesFlags::DEFAULT);
-//    imshow("sift_keypoint_image 1",img_kp_1);
-//    imshow("sift_keypoint_image 2",img_kp_2);
-//    /*特征向量提取*/
-//    Mat img1_descriptor,img2_descriptor;
-//    Ptr<DescriptorExtractor> sift_descriptor_extractor = DescriptorExtractor::create("SIFT");
-//    sift_descriptor_extractor->compute(img1_mat,img1_keypoint,img1_descriptor);
-//    sift_descriptor_extractor->compute(img2_mat,img2_keypoint,img2_descriptor);
-//    /*特征点匹配*/
-//    std::vector<DMatch> img_match;
-//    Ptr<DescriptorMatcher> bruteforce_matcher = DescriptorMatcher::create("BruteForce");
-//    bruteforce_matcher->match(img1_descriptor,img2_descriptor,img_match);
-//
-//    /*在图片上显示匹配结果*/
-//    Mat match_img;
-//    drawMatches(img1_mat,img1_keypoint,img2_mat,img2_keypoint,accurate_match,match_img);
-//    imshow("match image",match_img);
-//    waitKey(0);
+    initModule_nonfree();//初始化模块，使用SIFT或SURF时用到
+    Ptr<FeatureDetector> detector = FeatureDetector::create( "SIFT" );//创建SIFT特征检测器
+    Ptr<DescriptorExtractor> descriptor_extractor = DescriptorExtractor::create( "SIFT" );//创建特征向量生成器
+    Ptr<DescriptorMatcher> descriptor_matcher = DescriptorMatcher::create( "BruteForce" );//创建特征匹配器
+    if( detector.empty() || descriptor_extractor.empty() )
+    {
+    	cout<<"fail to create detector!";
+    }
+
+    //读入图像
+    Mat img1 = face1;
+    Mat img2 = face2;
+
+    //特征点检测
+    double t = getTickCount();//当前滴答数
+    vector<KeyPoint> keypoints1,keypoints2;
+    detector->detect( img1, keypoints1 );//检测img1中的SIFT特征点，存储到keypoints1中
+    detector->detect( img2, keypoints2 );
+    cout<<"图像1特征点个数:"<<keypoints1.size()<<endl;
+    cout<<"图像2特征点个数:"<<keypoints2.size()<<endl;
+
+    //根据特征点计算特征描述子矩阵，即特征向量矩阵
+    Mat descriptors1,descriptors2;
+    descriptor_extractor->compute( img1, keypoints1, descriptors1 );
+    descriptor_extractor->compute( img2, keypoints2, descriptors2 );
+    t = ((double)getTickCount() - t)/getTickFrequency();
+    cout<<"SIFT算法用时："<<t<<"秒"<<endl;
+
+    cout<<"图像1特征描述矩阵大小："<<descriptors1.size()
+        <<"，特征向量个数："<<descriptors1.rows<<"，维数："<<descriptors1.cols<<endl;
+    cout<<"图像2特征描述矩阵大小："<<descriptors2.size()
+        <<"，特征向量个数："<<descriptors2.rows<<"，维数："<<descriptors2.cols<<endl;
+
+    //画出特征点
+    Mat img_keypoints1,img_keypoints2;
+    drawKeypoints(img1,keypoints1,img_keypoints1,Scalar::all(-1),0);
+    drawKeypoints(img2,keypoints2,img_keypoints2,Scalar::all(-1),0);
+    imshow("Src1",img_keypoints1);
+    imshow("Src2",img_keypoints2);
+
+    //特征匹配
+    vector<DMatch> matches;//匹配结果
+    descriptor_matcher->match( descriptors1, descriptors2, matches );//匹配两个图像的特征矩阵
+    cout<<"Match个数："<<matches.size()<<endl;
+
+    //计算匹配结果中距离的最大和最小值
+    //距离是指两个特征向量间的欧式距离，表明两个特征的差异，值越小表明两个特征点越接近
+    double max_dist = 0;
+    double min_dist = 100;
+    for(int i=0; i<matches.size(); i++)
+    {
+        double dist = matches[i].distance;
+        if(dist < min_dist) min_dist = dist;
+        if(dist > max_dist) max_dist = dist;
+    }
+    cout<<"最大距离："<<max_dist<<endl;
+    cout<<"最小距离："<<min_dist<<endl;
+
+    //筛选出较好的匹配点
+    vector<DMatch> goodMatches;
+    for(int i=0; i<matches.size(); i++)
+    {
+        if(matches[i].distance < 0.31 * max_dist)
+        {
+            goodMatches.push_back(matches[i]);
+        }
+    }
+
+    //画出匹配结果
+    Mat img_matches;
+    //红色连接的是匹配的特征点对，绿色是未匹配的特征点
+    drawMatches(img1,keypoints1,img2,keypoints2,goodMatches,img_matches,
+                Scalar::all(-1)/*CV_RGB(255,0,0)*/,CV_RGB(0,255,0),Mat(),2);
+
+    imshow("MatchSIFT", img_matches);
+
+    cout<<"goodMatch个数："<<goodMatches.size()<<endl;
+    if(goodMatches.size() > 15)
+    {
+    	return 0;
+    }
+
+    return 1;
 }
 
 //处理原图尺寸
@@ -71,17 +123,25 @@ int resizeImage(const cv::Mat &srcMat, cv::Mat &destMat)
 	return 0;
 }
 
-int main( int argc, char** argv ){
-    std::string resPath = "D:\\eclipse-workspace\\MinGWDebugTest\\res\\";
-    //std::string resPath = ".\\res\\";
+//检测人脸区域
+int detectFaceRects(CascadeClassifier &detecter,
+		const cv::Mat &frameGray, vector<Rect> &vrectFaces)
+{
+	detecter.detectMultiScale( frameGray, vrectFaces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+	return 0;
+}
 
+int main( int argc, char** argv ){
+    std::string resPath = ".\\res\\";
+
+    if( argc != 3){
+        cout << "参数个数错误" << endl;
+        return -1;
+    }
+
+    string filenameFace1 = resPath + argv[1] + ".jpg";
+    string filenameFace2 = resPath + argv[2] + ".jpg";
 	string xmlPath = resPath + "haarcascade_frontalface_alt.xml";
-	CascadeClassifier ccf;   //创建分类器对象
-	if (!ccf.load(xmlPath))   //加载训练文件
-	{
-		cout << "不能加载文件" << xmlPath << endl;
-		return 0;
-	}
 
     Mat frameFace1;
     Mat frameFace2;
@@ -91,8 +151,6 @@ int main( int argc, char** argv ){
     Mat frameGray2;
     vector<Rect> vrectFace1;
     vector<Rect> vrectFace2;
-    string filenameFace1 = resPath + "pic17.jpg";
-    string filenameFace2 = resPath + "pic18.jpg";
 
     namedWindow(WINDOWNAME_FACE1);
     namedWindow(WINDOWNAME_FACE2);
@@ -129,15 +187,21 @@ int main( int argc, char** argv ){
     equalizeHist(frameGray2, frameGray2);  //直方图均衡行
 
 	//人脸区域检测
+	CascadeClassifier ccf;   //创建分类器对象
+	if (!ccf.load(xmlPath))   //加载训练文件
+	{
+		cout << "无法加载文件" << xmlPath << endl;
+		return 0;
+	}
     vrectFace1.clear();
     vrectFace2.clear();
-    ccf.detectMultiScale( frameGray1, vrectFace1, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+    detectFaceRects(ccf, frameGray1, vrectFace1);
     if(vrectFace1.size())
     {
     	rectangle(frameFace1, vrectFace1[0], Scalar(0,255,0));
     }
     imshow(WINDOWNAME_FACE1, frameFace1);
-    ccf.detectMultiScale( frameGray2, vrectFace2, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+    detectFaceRects(ccf, frameGray2, vrectFace2);
     if(vrectFace2.size())
     {
     	rectangle(frameFace2, vrectFace2[0], Scalar(0,255,0));
@@ -147,8 +211,8 @@ int main( int argc, char** argv ){
     //截取人脸图像
     Mat roiFace1 = frameGray1(vrectFace1[0]);
     Mat roiFace2 = frameGray2(vrectFace2[0]);
-    imshow(WINDOWNAME_FACE1, roiFace1);
-    imshow(WINDOWNAME_FACE2, roiFace2);
+//    imshow(WINDOWNAME_FACE1, roiFace1);
+//    imshow(WINDOWNAME_FACE2, roiFace2);
 
     //人脸匹配
     string result = "不是同一个人";
